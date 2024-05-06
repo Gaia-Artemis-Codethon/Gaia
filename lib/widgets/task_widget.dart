3f84edde28a91b594fa4ff4f6ee9e9f27a8fe3e9
LuisNavarroMontes <luis.navarro.montes@gmail.com>
2024-05-06 18:02:11 +0200

    to do un poco mas parecido a la imagen

--- lib/widgets/task_widget.dart
+++ lib/widgets/task_widget.dart
@@ -1,18 +1,19 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_huerto/const/colors.dart';
 import 'package:flutter_application_huerto/main.dart';
 import 'package:flutter_application_huerto/models/task.dart';
 import 'package:flutter_application_huerto/service/task_supabase.dart';
 import 'package:flutter_guid/flutter_guid.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import '../components/text_field_tasks.dart';
 import '../pages/toDo/edit_toDo.dart';
 
 class TaskWidget extends StatefulWidget {
   final Task note;
-  final VoidCallback onTaskStatusChanged; // Nuevo argumento para el callback
+  final VoidCallback onTaskStatusChanged;
 
-  const TaskWidget(this.note, this.onTaskStatusChanged, {super.key});
+  const TaskWidget(this.note, this.onTaskStatusChanged, {Key? key})
+      : super(key: key);
 
   @override
   State<TaskWidget> createState() => _TaskWidgetState();
@@ -21,148 +22,196 @@ class TaskWidget extends StatefulWidget {
 class _TaskWidgetState extends State<TaskWidget> {
   bool isDone = false;
   bool isDescriptionExpanded = false;
+  bool isEditingTitle = false; // Estado para controlar la edición del título
+  bool isEditingDescription = false; // Estado para controlar la edición de la descripción
   late String description;
+  late String title; // Variable para almacenar el título editado
 
   @override
   void initState() {
     super.initState();
     isDone = widget.note.status;
     description = widget.note.description ?? '';
+    title = widget.note.name;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-      child: Stack(
-        children: [
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.2),
-                  spreadRadius: 1,
-                  blurRadius: 3,
-                  offset: const Offset(0, 2),
-                ),
-              ],
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.grey.withOpacity(0.2),
-                                    spreadRadius: 1,
-                                    blurRadius: 3,
-                                    offset: const Offset(0, 2),
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isEditingTitle = false;
+          isEditingDescription = false;
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+        child: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween, // Alinear los iconos a la derecha
+                      children: [
+                        Flexible(
+                          child: GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                isEditingTitle = true;
+                                isEditingDescription = false; // Asegurarse de que la edición de la descripción esté desactivada
+                              });
+                            },
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                // Usa un Text hasta que se haga clic en él
+                                isEditingTitle
+                                    ? TextField(
+                                        controller: TextEditingController(text: title),
+                                        onChanged: (value) {
+                                          setState(() {
+                                            title = value;
+                                          });
+                                        },
+                                        onSubmitted: (value) {
+                                          setState(() {
+                                            isEditingTitle = false;
+                                          });
+                                          _updateTaskName();
+                                        },
+                                      )
+                                    : Text(
+                                        title,
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                        ),
+                                      ),
+                                SizedBox(height: 5),
+                                AnimatedCrossFade(
+                                  duration: Duration(milliseconds: 300),
+                                  crossFadeState: isDescriptionExpanded
+                                      ? CrossFadeState.showSecond
+                                      : CrossFadeState.showFirst,
+                                  firstChild: SizedBox.shrink(),
+                                  secondChild: GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        isEditingTitle = false;
+                                        isEditingDescription = true;
+                                      });
+                                    },
+                                    child: TextField(
+                                      controller: TextEditingController(text: description),
+                                      maxLines: null,
+                                      decoration: InputDecoration(
+                                        enabledBorder: InputBorder.none,
+                                        focusedBorder: InputBorder.none,
+                                      ),
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                        fontSize: 16,
+                                      ),
+                                      onChanged: (value) {
+                                        setState(() {
+                                          description = value;
+                                        });
+                                      },
+                                      onSubmitted: (value) {
+                                        setState(() {
+                                          isEditingDescription = false;
+                                        });
+                                        _updateTaskDescription();
+                                      },
+                                    ),
                                   ),
-                                ],
-                              ),
-                              child: Text(
-                                widget.note.name,
-                                style: const TextStyle(
-                                  fontSize: 18,
                                 ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  isEditingTitle = false; // Ocultar la barra de edición del título
+                                  isEditingDescription = false; // Ocultar la barra de edición de la descripción
+                                });
+                                TaskSupabase().updateTask(Task(
+                                  id: widget.note.id,
+                                  name: title,
+                                  status: widget.note.status,
+                                  description: description,
+                                  user_id: widget.note.user_id,
+                                ));
+                              },
+                              child: Icon(
+                                Icons.edit,
+                                color: Colors.blue,
                               ),
                             ),
-                            SizedBox(height: 5),
-                            AnimatedCrossFade(
-                              duration: Duration(milliseconds: 300),
-                              crossFadeState: isDescriptionExpanded
-                                  ? CrossFadeState.showSecond
-                                  : CrossFadeState.showFirst,
-                              firstChild: SizedBox.shrink(),
-                              secondChild: Text(
-                                description,
-                                style: TextStyle(color: Colors.grey),
+                            SizedBox(width: 8),
+                            GestureDetector(
+                              onTap: () {
+                                showConfirmationDialog(context);
+                              },
+                              child: Icon(
+                                Icons.delete,
+                                color: Colors.red,
+                              ),
+                            ),
+                            SizedBox(width: 8),
+                            Checkbox(
+                              activeColor: Colors.green.shade200,
+                              value: isDone,
+                              onChanged: (value) async {
+                                setState(() {
+                                  isDone = !isDone;
+                                });
+                                await TaskSupabase().taskIsDone(Task(
+                                  id: widget.note.id,
+                                  status: isDone,
+                                  name: title,
+                                  description: description,
+                                  user_id: widget.note.user_id,
+                                ));
+                                widget.onTaskStatusChanged();
+                              },
+                            ),
+                            SizedBox(width: 8),
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  isDescriptionExpanded = !isDescriptionExpanded;
+                                });
+                              },
+                              child: Icon(
+                                isDescriptionExpanded
+                                    ? Icons.keyboard_arrow_up
+                                    : Icons.keyboard_arrow_down,
+                                color: Colors.grey,
                               ),
                             ),
                           ],
                         ),
-                      ),
-                      Row(
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => EditToDo(
-                                  widget.note,
-                                  userId!,
-                                  widget.onTaskStatusChanged,
-                                ),
-                              ));
-                            },
-                            child: Icon(
-                              Icons.edit,
-                              color: Colors.blue,
-                            ),
-                          ),
-                          SizedBox(width: 8),
-                          GestureDetector(
-                            onTap: () {
-                              showConfirmationDialog(context);
-                            },
-                            child: Icon(
-                              Icons.delete,
-                              color: Colors.red,
-                            ),
-                          ),
-                          SizedBox(width: 8),
-                          Checkbox(
-                            activeColor: Colors.green.shade200,
-                            value: isDone,
-                            onChanged: (value) async {
-                              setState(() {
-                                isDone = !isDone;
-                              });
-                              await TaskSupabase().taskIsDone(Task(
-                                id: widget.note.id,
-                                status: isDone,
-                                name: widget.note.name,
-                                description: description,
-                                user_id: widget.note.user_id,
-                              ));
-                              widget.onTaskStatusChanged();
-                            },
-                          ),
-                          SizedBox(width: 8),
-                          GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                isDescriptionExpanded = !isDescriptionExpanded;
-                              });
-                            },
-                            child: Icon(
-                              isDescriptionExpanded
-                                  ? Icons.keyboard_arrow_up
-                                  : Icons.keyboard_arrow_down,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                editTime(),
-              ],
+                  editTime(),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -231,5 +280,25 @@ class _TaskWidgetState extends State<TaskWidget> {
         );
       },
     );
+  }
+
+  void _updateTaskName() {
+    TaskSupabase().updateTask(Task(
+      id: widget.note.id,
+      name: title,
+      status: widget.note.status,
+      description: description,
+      user_id: widget.note.user_id,
+    ));
+  }
+
+  void _updateTaskDescription() {
+    TaskSupabase().updateTask(Task(
+      id: widget.note.id,
+      name: title,
+      status: widget.note.status,
+      description: description,
+      user_id: widget.note.user_id,
+    ));
   }
 }
