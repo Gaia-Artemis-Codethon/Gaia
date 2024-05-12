4a64240513356b973161a166dfb7a4803f4caaa6
Gabriel <correoclasegabri@gmail.com>
2024-05-12 19:31:14 +0200

    Persistent colors

--- lib/pages/drawing/create_grid.dart
+++ lib/pages/drawing/create_grid.dart
@@ -2,9 +2,11 @@ import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_application_huerto/components/button.dart';
 import 'package:flutter_application_huerto/const/colors.dart';
+import 'package:flutter_application_huerto/models/sketch.dart';
 import 'package:flutter_application_huerto/service/grid_supabase.dart';
 import 'package:flutter_guid/flutter_guid.dart';
 
@@ -27,7 +29,7 @@ class _GridPageState extends State<GridPage> {
     Owner("Luis", Colors.yellow, <int>[]),
   ];
 
-  Owner currentOwner = Owner("Artemis", Colors.grey, <int>[]);
+  Owner currentOwner = Owner("Artemis", Colors.white, <int>[]);
 
   Map<Owner, List<int>> belongsTo = Map<Owner, List<int>>();
   static const DEFAULT_GRID_SIZE = 100;
@@ -55,7 +57,15 @@ class _GridPageState extends State<GridPage> {
       gridDao = data!;
       owners = <Owner>[];
       gridDao.tileDistribution.forEach((key, value) {
-        owners.add(Owner(key, getRandomColor(), value));
+        if(key.contains("-")){
+          String currentColor = key.substring(key.indexOf("-")+1,key.length);
+          print(currentColor);
+          Color c = getColorFromHex(currentColor);
+          owners.add(Owner(key, c, value));
+        }else{
+          Color c = getRandomColor();
+          owners.add(Owner(key+"-"+c.toHex(), getRandomColor(), value));
+        }
       });
       build(context);
       print("Data: ${gridDao.communityId}");
@@ -68,7 +78,29 @@ class _GridPageState extends State<GridPage> {
       _getGridData();
     }
   }
+  void _changeGridSize(int x, int y) async{
+    Map<String,List<int>> clearTileDistribution = {};
+    gridDao.tileDistribution.forEach((key, value) {
+      clearTileDistribution[key] = [];
+    });
 
+    GridDto newData= GridDto(
+      id: gridDao.id,
+      communityId: gridDao.communityId,
+      dimensionsX: x,
+      dimensionsY: y,
+      tileDistribution: clearTileDistribution,
+    );
+    bool isUpdated = await GridSupabase().updateGridDataByCommunityId(gridDao);
+
+  }
+
+  Color getColorFromHex(String hexColor) {
+    hexColor = hexColor.replaceAll('#', '');
+    int hexValue = int.parse(hexColor, radix: 16);
+    return Color(hexValue).withOpacity(1.0);
+  }
+  
   Color getRandomColor() {
     Random random = Random();
     Color targetColor = OurColors().primaryButton;
@@ -76,11 +108,11 @@ class _GridPageState extends State<GridPage> {
 
     Color generatedColor;
     do {
-      generatedColor = Color.fromRGBO(
-        random.nextInt(256), // Red value
-        random.nextInt(256), // Green value
-        random.nextInt(256), // Blue value
-        1.0, // Alpha value (fully opaque)
+      generatedColor = Color.fromRGBO( //128+random(128) generates pastel colors instead of eyeball bleeding
+        128+random.nextInt(128), // Red value
+        128+random.nextInt(128), // Green value
+        128+random.nextInt(128), // Blue value
+        1.0,
       );
     } while (_colorDifference(generatedColor, targetColor) < threshold);
 
@@ -97,8 +129,22 @@ class _GridPageState extends State<GridPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
           title: Text("Community's farm"),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return InputDialog();
+                  },
+                );
+              },
+            ),
+          ],
         ),
         body: gridDao.id != Guid.defaultValue
             ? Column(
@@ -131,17 +177,17 @@ class _GridPageState extends State<GridPage> {
                                 color: getColorByOwner(
                                     index), // Change color when pressed
                                 borderRadius: BorderRadius.circular(5),
-                                border: Border.all(color: Colors.blueAccent)),
+                                border: Border.all(color: Colors.blue, width: 2)),
                             child: Text("${index}",
                                 style: TextStyle(
-                                    color: Colors.black,
+                                    color: Colors.white,
                                     fontSize: 16,
                                     fontWeight: FontWeight.bold)),
                           ),
                         );
                       }),
                   SizedBox(
-                      height: 350,
+                      height: 275,
                       child: gridDao.communityId != Guid.defaultValue
                           ? ListView.builder(
                               scrollDirection: Axis.vertical,
@@ -156,7 +202,7 @@ class _GridPageState extends State<GridPage> {
                                         });
                                       }
                                     },
-                                    text: Text(owners[index].name),
+                                    text: Text(owners[index].name.substring(0,owners[index].name.indexOf("-"))),
                                     style: ButtonStyle(
                                       backgroundColor: MaterialStateProperty.all(owners[index].getColor()),
                                       foregroundColor: MaterialStateProperty.all(getFontColorForText(owners[index].getColor()))
@@ -165,49 +211,63 @@ class _GridPageState extends State<GridPage> {
                               },
                             )
                           : Container(child: CircularProgressIndicator())),
-                  session.isAdmin ?
-                      Button(
-                        text: Text(
-                            "Save changes",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20
-                            ),),
-                        style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all(OurColors().primaryButton),
-                            foregroundColor: MaterialStateProperty.all(Colors.black),
-                        ),
-                        onPressed: (){
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                title: Text('Save changes'),
-                                content: Text('Are you sure you want to update the distribution?'),
-                                actions: <Widget>[
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: Text('Cancel'),
-                                  ),
-                                  TextButton(
-                                    onPressed: () {
-                                      _updateGridData();
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: Text('Yes'),
-                                  ),
-                                ],
-                              );
-                            },
-                          );
-                        },
-                      ) :
-                      Container()
                 ],
               )
-            : CircularProgressIndicator());
+            : CircularProgressIndicator()
+        ,
+
+    floatingActionButtonLocation:
+    FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Container(
+        height: 50,
+        margin: const EdgeInsets.all(10),
+        child:
+        session.isAdmin ?
+        ElevatedButton(
+          onPressed: () {},
+          child: Center(
+            child:                       Button(
+              text: Text(
+                "Save changes",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20
+                ),),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(OurColors().primaryButton),
+                foregroundColor: MaterialStateProperty.all(Colors.black),
+              ),
+              onPressed: (){
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text('Save changes'),
+                      content: Text('Are you sure you want to update the distribution?'),
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Cancel'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            _updateGridData();
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Yes'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            )
+          ),
+        ):Container(),
+      ),
+    );
   }
 
   Color getFontColorForText(Color background) {
@@ -215,7 +275,7 @@ class _GridPageState extends State<GridPage> {
   }
 
   Color getColorByOwner(int index) {
-    Color c = Colors.black;
+    Color c = Colors.white;
     owners.forEach((owner) {
       owner.getProperties().forEach((property) {
         if (property == index) {
@@ -224,5 +284,101 @@ class _GridPageState extends State<GridPage> {
       });
     });
     return c;
+  }
+}
+
+class InputDialog extends StatefulWidget {
+  @override
+  _InputDialogState createState() => _InputDialogState();
+}
+
+class _InputDialogState extends State<InputDialog> {
+  TextEditingController _x = TextEditingController();
+  TextEditingController _y = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Enter Data'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          TextFormField(
+            controller: _x,
+            decoration: InputDecoration(labelText: 'Plot Width'),
+            keyboardType: TextInputType.number,
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(
+                  RegExp(r'^[1-9]|10$')), // Only allow numbers from 1 to 10
+            ],
+          ),
+          TextFormField(
+            controller: _y,
+            decoration: InputDecoration(labelText: 'Plot Height'),
+            keyboardType: TextInputType.number,
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(
+                  RegExp(r'^[1-9]|10$')), // Only allow numbers from 1 to 10
+            ],
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        TextButton(
+          onPressed: () {
+            // Close the dialog without performing any action
+            Navigator.of(context).pop();
+          },
+          child: Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () {
+            // Close the dialog and show a new dialog if conditions are met
+            if (_isValidInput()) {
+              Navigator.of(context).pop();
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('WARNING!'),
+                    content: Text('By changing the size of the plot the distribution will be erased.\nDo you really want to continue?.'),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Cancel'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                          if(int.parse(_x.text)>1 && int.parse(_x.text)<10 && int.parse(_y.text)>1 && int.parse(_y.text)<10){ //Just in case
+                            //_changeGridSize(int.parse(_x.text),int.parse(_y.text));
+                          }
+                        },
+                        child: Text('OK'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            } else {
+
+            }
+          },
+          child: Text('OK'),
+        ),
+      ],
+    );
+  }
+  bool _isValidInput() {
+    return _x.text.isNotEmpty && _y.text.isNotEmpty;
+  }
+
+  @override
+  void dispose() {
+    _x.dispose();
+    _y.dispose();
+    super.dispose();
   }
 }
