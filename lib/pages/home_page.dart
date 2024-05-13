a8877c45ecd812550a9ed188b63b5cf2410ad136
dani <danielsanzarilla2011@gmail.com>
2024-05-13 19:23:37 +0200

    texto traducido

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -237,7 +237,7 @@ class _HomePageState extends State<HomePage> {
                   ),
                   const SizedBox(height: 10),
                   const Text(
-                    "My Land",
+                    "Our Plot",
                     style: TextStyle(
                         color: Colors.black,
                         fontSize: 30,
