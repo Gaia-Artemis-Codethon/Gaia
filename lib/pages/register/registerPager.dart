dce25839c17f8d9fe266461db41c6e14ffc90e29
dani <danielsanzarilla2011@gmail.com>
2024-05-16 14:28:18 +0200

    reg val

--- lib/pages/register/registerPager.dart
+++ lib/pages/register/registerPager.dart
@@ -4,7 +4,6 @@ import 'package:flutter_application_huerto/pages/login/loginPage.dart';
 import 'package:flutter_application_huerto/service/user_supabase.dart';
 import 'package:flutter_guid/flutter_guid.dart';
 import 'package:supabase/supabase.dart';
-
 import '../../models/userLoged.dart';
 import '../../service/supabaseService.dart';
 
@@ -14,11 +13,19 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final GlobalKey<FormFieldState> _nameKey = GlobalKey<FormFieldState>();
+  final GlobalKey<FormFieldState> _emailKey = GlobalKey<FormFieldState>();
+  final GlobalKey<FormFieldState> _passwordKey = GlobalKey<FormFieldState>();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _nameController = TextEditingController();
 
   Future<void> _register() async {
+    if (!_formKey.currentState!.validate()) {
+      return; // Si el formulario no es válido, detener la operación
+    }
+
     String email = _emailController.text.trim();
     String password = _passwordController.text.trim();
     String name = _nameController.text.trim();
@@ -83,84 +90,120 @@ class _RegisterPageState extends State<RegisterPage> {
               ),
             ),
             Positioned(
-              top: 145, // Ajusta la posición según sea necesario
+              top: 145,
               left: 20,
               right: 20,
-              child: Container(
-                margin: EdgeInsets.symmetric(
-                    horizontal: 24.0), // Ajusta el margen lateral
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      'Create Account',
-                      style: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    SizedBox(height: 20.0),
-                    TextField(
-                      controller: _nameController,
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white.withOpacity(0.7),
-                        labelText: 'Name',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
+              child: Form(
+                key: _formKey,
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        'Create Account',
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
                         ),
                       ),
-                    ),
-                    SizedBox(height: 15.0),
-                    TextField(
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white.withOpacity(0.7),
-                        labelText: 'Email',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
+                      SizedBox(height: 20.0),
+                      TextFormField(
+                        key: _nameKey,
+                        controller: _nameController,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white.withOpacity(0.7),
+                          labelText: 'Name',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
                         ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your name';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          _nameKey.currentState!.validate();
+                        },
                       ),
-                    ),
-                    SizedBox(height: 15.0),
-                    TextField(
-                      controller: _passwordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white.withOpacity(0.7),
-                        labelText: 'Password',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
+                      SizedBox(height: 15.0),
+                      TextFormField(
+                        key: _emailKey,
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white.withOpacity(0.7),
+                          labelText: 'Email',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
                         ),
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              !value.contains('@')) {
+                            return 'Please enter a valid email';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          _emailKey.currentState!.validate();
+                        },
                       ),
-                    ),
-                    SizedBox(height: 30.0),
-                    SizedBox(
-                      width: double.infinity,
-                      height: 45.0, // Match the height of the TextField
-                      child: ElevatedButton(
-                        onPressed: _register,
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              OurColors().primeWhite),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15.0),
+                      SizedBox(height: 15.0),
+                      TextFormField(
+                        key: _passwordKey,
+                        controller: _passwordController,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white.withOpacity(0.7),
+                          labelText: 'Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                        validator: (value) {
+                          if (value == null ||
+                              value.isEmpty ||
+                              value.length < 6) {
+                            return 'Password must be at least 6 characters';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          _passwordKey.currentState!.validate();
+                        },
+                      ),
+                      SizedBox(height: 30.0),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 45.0,
+                        child: ElevatedButton(
+                          onPressed: _register,
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                OurColors().primeWhite),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                            ),
+                          ),
+                          child: Text(
+                            'Register',
+                            style: TextStyle(
+                              color: Colors.black,
                             ),
                           ),
                         ),
-                        child: Text(
-                          'Register',
-                          style: TextStyle(
-                            color: Colors.black,
-                          ),
-                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
